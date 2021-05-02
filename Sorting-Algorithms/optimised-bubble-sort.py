c81cecad7e08b1dcb542289d356e92b0d8ce4c4d
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-05-02 14:33:09 +0530

    Create optimised-bubble-sort.py

---
+++ Sorting-Algorithms/optimised-bubble-sort.py
@@ -0,0 +1,14 @@
+def optimised_bubble_sort(arr):
+    for i in range(len(arr)):
+        swapped = False
+        for j in range(0,len(arr)-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swapped = True
+        if swapped == False:
+            break
+    return arr
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+print(optimised_bubble_sort(arr))
+
