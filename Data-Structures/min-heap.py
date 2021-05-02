e1843e60af24997cff9e74a8677adcc1241675d7
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-05-02 19:14:37 +0530

    Update min-heap.py

--- Data-Structures/min-heap.py
+++ Data-Structures/min-heap.py
@@ -2,7 +2,7 @@ import heapq
 
 min_heap1 = [5, 7, 9, 1, 3]
 
-# using heapq.heapify(list) to convert list into heap
+# using heapq.heapify(list) to convert list into min heap
 heapq.heapify(min_heap1)
 
 heapq.heappush(min_heap1, 4)
@@ -18,8 +18,8 @@ min_heap3 = [5, 7, 9, 4, 3]
 heapq.heapify(min_heap2)
 heapq.heapify(min_heap3)
 
-# heapq.heappushpop(heap, item) and heapq.heapreplace(heap, item) are used to push and pop items simultaneously
-# heapq.heapreplace(heap, item) returns the smallest value originally in heap regardless of the pushed element as opposed to heapq.heappushpop(heap, item).
+# heapq.heappushpop(min_heap, item) and heapq.heapreplace(min_heap, item) are used to push and pop items simultaneously
+# heapq.heapreplace(min_heap, item) returns the smallest value originally in heap regardless of the pushed element as opposed to heapq.heappushpop(min_heap, item).
 heapq.heappushpop(min_heap2, 2)
 # pops 2
 
