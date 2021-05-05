7f776198e9f06d1f35d1fe03fb58614bc5c4394c
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-05-05 10:44:58 +0530

    Create binary-search.py

---
+++ Searching-Algorithms/binary-search.py
@@ -0,0 +1,18 @@
+def binary_search(arr, startIndex, endIndex, x):
+    midIndex = (startIndex + endIndex)//2
+    if startIndex > endIndex:
+        return -1
+    else:
+        if arr[midIndex] == x:
+            return midIndex
+        elif arr[midIndex] > x:
+            return binary_search(arr, startIndex, midIndex-1, x)
+        else:
+            return binary_search(arr, midIndex+1, endIndex, x)
+
+arr = [1,2,3,4,5,6]
+print(binary_search(arr,0,len(arr),6))
+arr = [ 2, 3, 4, 10, 40 ]
+print(binary_search(arr,0,len(arr),10))
+    
+ 
