c820f25249b3c9f9edce8cc01d561195fabba0a5
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-05-20 19:06:22 +0530

    Create postfix-to-infix.py

---
+++ DSA-Questions/postfix-to-infix.py
@@ -0,0 +1,13 @@
+OPERATORS = set(['+','-','/','*','(',')','%','^'])
+def postfix_to_infix(expression):
+    stack = []
+    for ch in expression:
+        if ch not in OPERATORS:
+            stack.append(ch)
+        else:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            stack.append("("+val2+ch+val1+")")
+    return stack.pop()
+
+print(postfix_to_infix("ab*c+d*e/"))
