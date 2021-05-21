8cd28a54a89473d3c29831ba2dada3622e5f853f
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-05-21 11:36:41 +0530

    Update queue-using-stacks.py

--- DSA-Questions/queue-using-stacks.py
+++ DSA-Questions/queue-using-stacks.py
@@ -13,10 +13,15 @@ class Queue:
         if not self.stack2:
             while self.stack1:
                 self.stack2.append(self.stack1.pop())
-        return self.stack2.pop()
+        if self.stack2:
+            return self.stack2.pop()
+        else:
+            return -1
 
 
 queue1 = Queue()
+print(queue1.deQueue())
+print(queue1.isEmpty())
 queue1.enQueue(10)
 queue1.enQueue(20)
 queue1.enQueue(30)
